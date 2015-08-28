06d5617cdd2b0f5f414d7a9b7a9c84b4536b2d6b
Nicolas <nicolas.mas@gmail.com>
2015-08-28 14:13:40 +0800

    Initial Commit
    Initial Commit

---
+++ files/dummy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+#
+# This is a dummy demo script to demo supervisord
+# It just print "ping !" in /tmp/ping.txt evey
+# 5 second
+
+echo "ping !" >> /tmp/ping.txt
+sleep 5
+
+exit 0
